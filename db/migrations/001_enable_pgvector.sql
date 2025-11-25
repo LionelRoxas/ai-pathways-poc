f24ea493d30001a1df0a7d0c807cde548a03a6fc
Lionel Derrick Roxas <lionelroxas80@gmail.com>
2025-11-24 20:20:32 -1000

    Configure Git LFS for large data files
    - Add .gitattributes for JSON, JSONL, and other large file types
    - Track db/programs_with_embeddings.json with LFS
    - Track SQL migration files with LFS
    - Set up LFS for future large files (models, databases, archives)

---
+++ db/migrations/001_enable_pgvector.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6718a7757fc1eb8086095cd78f9e2371e7ebb8957f96ab3b2e5fea6f70b8738
+size 395
---
+++ db/migrations/002_create_programs_table.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4925868f65da27a7b52ac58252a8df1f06e2e154129b99368e450fbe7b22533b
+size 3525
